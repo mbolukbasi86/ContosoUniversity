4a1e2e410fb50913365b6883886ba6e5935a6c0f
Murat Bolukbasi <mbolukbasi@protel.com.tr>
2022-02-14 08:20:59 +0300

    Add project files.

---
+++ ContosoUniversity.UI/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using ContosoUniversity.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.UI.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
---
+++ ContosoUniversity.UI/Program.cs
@@ -0,0 +1,50 @@
+using ContosoUniversity.Data.Context;
+using ContosoUniversity.Data.Initializer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExist(host);
+            host.Run();
+        }
+
+        public static void CreateDbIfNotExist(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DBInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "CreateDbIfNotExist Failed.!");
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
